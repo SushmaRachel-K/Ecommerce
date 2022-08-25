807f756f0fa4758b82725cd9d009e8b014b10506
SushmaRachel-K <66865600+SushmaRachel-K@users.noreply.github.com>
2022-08-25 12:23:12 +0530

    Add files via upload

---
+++ cart.php
@@ -0,0 +1,122 @@
+<?php
+
+require "config/constants.php";
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Ecommerce</title>
+		<link rel="stylesheet" href="css/bootstrap.min.css"/>
+		<script src="js/jquery2.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="main.js"></script>
+		<link rel="stylesheet" type="text/css" href="style.css"/>
+		<style>
+			.btn {
+	background-color: #4CAF50; /* Green */
+	border: none;
+	color: white;
+	padding: 16px 32px;
+	text-align: center;
+	text-decoration: none;
+	display: inline-block;
+	font-size: 16px;
+	margin: 4px 2px;
+	transition-duration: 0.4s;
+	cursor: pointer;
+  }
+  
+  .btn1 {
+	background-color: white; 
+	color: black; 
+	border: 2px solid #4CAF50;
+  }
+  
+  .btn1:hover {
+	background-color: #4CAF50;
+	color: white;
+  }
+  </style>
+	</head>
+<body>
+<div class="wait overlay">
+	<div class="loader"></div>
+</div>
+	<div class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container-fluid">	
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse" aria-expanded="false">
+					<span class="sr-only">navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a href="#" class="navbar-brand">Ecommerce</a>
+			</div>
+		<div class="collapse navbar-collapse" id="collapse">
+			<ul class="nav navbar-nav">
+				<li><a href="index.php"><span class="glyphicon glyphicon-home"></span>Home</a></li>
+				<li><a href="index.php"><span class="glyphicon glyphicon-modal-window"></span>Product</a></li>
+			</ul>
+		</div>
+	</div>
+	</div>
+	<p><br/></p>
+	<p><br/></p>
+	<p><br/></p>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-2"></div>
+			<div class="col-md-8" id="cart_msg">
+				<!--Cart Message--> 
+			</div>
+			<div class="col-md-2"></div>
+		</div>
+		<div class="row">
+			<div class="col-md-2"></div>
+			<div class="col-md-8">
+				<div class="panel panel-primary">
+					<div class="panel-heading">Cart Checkout</div>
+					<div class="panel-body">
+						<div class="row">
+							<div class="col-md-2 col-xs-2"><b>Action</b></div>
+							<div class="col-md-2 col-xs-2"><b>Product Image</b></div>
+							<div class="col-md-2 col-xs-2"><b>Product Name</b></div>
+							<div class="col-md-2 col-xs-2"><b>Quantity</b></div>
+							<div class="col-md-2 col-xs-2"><b>Product Price</b></div>
+							<div class="col-md-2 col-xs-2"><b>Price in <?php echo CURRENCY; ?></b></div>
+							
+						</div>
+						<div id="cart_checkout"></div>
+						<div class="btn btn1" style="margin-left:550px;" ><a style="text-decoration:none;" href="index.php">CHECK OUT</a></div>
+						</div> 
+					</div>
+					<div class="panel-footer"></div>
+				</div>
+			</div>
+		
+			
+		</div>
+
+<script>var CURRENCY = '<?php echo CURRENCY; ?>';</script>
+</body>	
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
